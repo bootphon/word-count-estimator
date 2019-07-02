9822b7b5ab7b69c77f228e7b8391e140450554c7
Mederic <carriatmederic@gmail.com>
2019-07-02 13:15:11 +0300

    Change load_features_batch.py parameters

--- src/features/load_features_batch.py
+++ src/features/load_features_batch.py
@@ -7,27 +7,24 @@ import numpy as np
 import scipy.io
 
 
-def cut_features_in_batches(features, model_file, window_step, window_length):
-    
-    # load meme, devi, ws
-    
+def cut_features_in_batches(features, model_params_file):
+   
     n_files = len(features)
-    tot_length = sum(len(x) for x in features)
+    tot_length = sum(len(f) for f in features)
     
     tot_features = np.concatenate(features)
-    timestamps = np.zeros(tot_length)
     
-    i = 0
-    l = 0
+    timestamps = np.zeros(tot_length)
+    i, l = 0, 0
     for f in features:
         file_length = len(f)
         timestamps[l:l+file_length] = [i]*file_length
         i += 1
         l += file_length
-        
-    model_params = scipy.io.loadmat(model_file)
-    wl = model_params['wl']
-    ws = model_params['ws']
+    
+    model_params = scipy.io.loadmat(model_params_file)
+    window_length = model_params['wl'][0][0]
+    window_step = model_params['ws'][0][0]
     meme = model_params['meme']
     devi = model_params['devi']
     
@@ -44,11 +41,11 @@ def cut_features_in_batches(features, model_file, window_step, window_length):
         timestamps = np.concatenate((timestamps, np.full((window_length-excess), i-1)))
         tot_length = len(tot_features)
     
-    total_slices = ((tot_length - window_length) // window_step) + 1
+    n_slices = ((tot_length - window_length) // window_step) + 1
     
     k = 0
-    X = np.zeros((total_slices, window_length, 24))
-    X_timestamps = np.zeros((total_slices, window_length))
+    X = np.zeros((n_slices, window_length, 24))
+    X_timestamps = np.zeros((n_slices, window_length))
     for i_start in range(0, tot_length - window_length + window_step, window_step): # add window step to upper bound because otherwise stops one step before
         X[k, :, :] = tot_features[i_start:i_start + window_length, :]
         X_timestamps[k, :] = timestamps[i_start:i_start+window_length]
@@ -62,4 +59,4 @@ def cut_features_in_batches(features, model_file, window_step, window_length):
     
     return X, X_timestamps
     
-X, X_ts = cut_features_in_batches(F, "C:/Users/Médéric Carriat/Desktop/wce/models/LSTM_params_BLSTM_fourlang_60_60_augmented_dropout_v2.h5.mat", 100, 300)
+X, X_ts = cut_features_in_batches(F, "C:/Users/Médéric Carriat/Desktop/wce/models/LSTM_params_BLSTM_fourlang_60_60_augmented_dropout_v2.h5.mat")
