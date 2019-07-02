c267d1a73df7f25c79ff3ddd0f5ebd5ec748be79
Mederic <carriatmederic@gmail.com>
2019-07-02 15:45:16 +0300

    Clarify variables' name

--- src/features/load_features_batch.py
+++ src/features/load_features_batch.py
@@ -2,25 +2,28 @@
 TODO
 """
 
-import math
 import numpy as np
 import scipy.io
 
 
-def cut_features_in_batches(features, model_params_file):
-   
-    n_files = len(features)
-    tot_length = sum(len(f) for f in features)
+def cut_features(features, model_params_file):
+    """
+    Cut all file's features into portions of equal size that can be
+    processed by the BLSTM network.
+    """
     
+    # join every file's features matrix together in one matrix
     tot_features = np.concatenate(features)
+    tot_length = len(tot_features)
     
-    timestamps = np.zeros(tot_length)
-    i, l = 0, 0
+    # features_ids keeps track of the number of the origin file of the features
+    features_ids = np.zeros(tot_length)
+    file_id, l = 0, 0
     for f in features:
-        file_length = len(f)
-        timestamps[l:l+file_length] = [i]*file_length
-        i += 1
-        l += file_length
+        n_features = len(f)
+        features_ids[l:l+n_features] = [file_id]*n_features
+        file_id += 1
+        l += n_features
     
     model_params = scipy.io.loadmat(model_params_file)
     window_length = model_params['wl'][0][0]
@@ -31,24 +34,25 @@ def cut_features_in_batches(features, model_params_file):
     tot_features = tot_features - meme
     tot_features = tot_features / devi
     
-    # Make sure that the length of the data is equal to multiple of window
-    # length to avoid any clipping of endings then slice the total features
-    # array
+    # add zeros if the length is not a multiple of the step's size
     excess = tot_length % window_step
     
     if excess != 0:
-        tot_features = np.concatenate((tot_features, np.zeros((window_length-excess, 24))))
-        timestamps = np.concatenate((timestamps, np.full((window_length-excess), i-1)))
+        tot_features = np.concatenate((tot_features,
+                                       np.zeros((window_length-excess, 24))))
+        features_ids = np.concatenate((features_ids,
+                                     np.full((window_length-excess), i-1)))
         tot_length = len(tot_features)
     
+    # slide window over total_features and add the result to X
     n_slices = ((tot_length - window_length) // window_step) + 1
     
     k = 0
     X = np.zeros((n_slices, window_length, 24))
-    X_timestamps = np.zeros((n_slices, window_length))
-    for i_start in range(0, tot_length - window_length + window_step, window_step): # add window step to upper bound because otherwise stops one step before
+    X_features_ids = np.zeros((n_slices, window_length))
+    for i_start in range(0, tot_length - window_length + window_step, window_step):
         X[k, :, :] = tot_features[i_start:i_start + window_length, :]
-        X_timestamps[k, :] = timestamps[i_start:i_start+window_length]
+        X_features_ids[k, :] = features_ids[i_start:i_start+window_length]
         k += 1
     
     X[np.isneginf(X)] = 0
@@ -56,7 +60,8 @@ def cut_features_in_batches(features, model_params_file):
     X[X == np.NaN] = 0
 
     # divide into multiple files if needed
+    # TODO
     
-    return X, X_timestamps
+    return X, X_features_ids
     
-X, X_ts = cut_features_in_batches(F, "C:/Users/Médéric Carriat/Desktop/wce/models/LSTM_params_BLSTM_fourlang_60_60_augmented_dropout_v2.h5.mat")
+X, X_ts = cut_features(F, "C:/Users/Médéric Carriat/Desktop/wce/models/LSTM_params_BLSTM_fourlang_60_60_augmented_dropout_v2.h5.mat")
