9ab09d701bf037df2c99a2ee3c8a3be8ddf6eaad
Mederic <carriatmederic@gmail.com>
2019-07-05 13:36:20 +0300

    Clarify variables' names

--- src/syllabification/features/cut_features.py
+++ src/syllabification/features/cut_features.py
@@ -6,29 +6,31 @@ import numpy as np
 import scipy.io
 
 
-def cut_features(features, model_params_file):
+def cut_features(feature_frames, model_params_file):
     """
         Cut all file's features into portions of equal size that can be
         processed by the BLSTM network.
     """
     
     # join every file's features matrix together in one matrix
-    tot_features = np.concatenate(features)
+    tot_features = np.concatenate(feature_frames)
     tot_length = len(tot_features)
     
-    # timestamps keeps track of the number of the origin file of the features
-    # and the timestamps of the features
-    timestamps = np.zeros((tot_length, 2))
-    frames_length = np.zeros(len(features))
-    file_id, l = 0, 0
-    for f in features:
+    # timestamps keeps track of the number of the origin feature frame and the
+    # timestamps of the features
+    timestamps = np.zeros((tot_length, 2), dtype=int)
+    frames_length = np.zeros(len(feature_frames), dtype=int)
+    frame_nb = 0
+    l = 0
+    for f in feature_frames:
         frame_len = len(f)
-        frames_length[file_id] = frame_len
+        frames_length[frame_nb] = frame_len
         for i in range(l, l+frame_len):
-            timestamps[i] = (file_id, i-l)
-        file_id += 1
+            timestamps[i] = (frame_nb, i-l)
+        frame_nb += 1
         l += frame_len
     
+    # load parameters for window, meme/devi matrices and apply them
     model_params = scipy.io.loadmat(model_params_file)
     window_length = model_params['wl'][0][0]
     window_step = model_params['ws'][0][0]
@@ -40,21 +42,19 @@ def cut_features(features, model_params_file):
     
     # add zeros if the length is not a multiple of the step's size
     excess = tot_length % window_step
-    
     if excess != 0:
         tot_features = np.concatenate((tot_features,
                                        np.zeros((window_length-excess, 24))))
         timestamps = np.concatenate((timestamps,
                                        np.full((window_length-excess, 2),
-                                               (file_id-1, -1))))
+                                               (frame_nb-1, -1))))
         tot_length = len(tot_features)
     
-    # slide window over total_features and add the result to X
+    # slide window over total_features and append it to X at each step
     n_slices = ((tot_length - window_length) // window_step) + 1
-    
-    k = 0
     X = np.zeros((n_slices, window_length, 24))
-    X_timestamps = np.zeros((n_slices, window_length, 2))
+    X_timestamps = np.zeros((n_slices, window_length, 2), dtype=int)
+    k = 0
     for i_start in range(0, tot_length-window_length+window_step, window_step):
         X[k, :, :] = tot_features[i_start:i_start + window_length, :]
         X_timestamps[k, :] = timestamps[i_start:i_start+window_length, :]
--- src/syllabification/features/generate_features.py
+++ src/syllabification/features/generate_features.py
@@ -7,9 +7,9 @@ import numpy as np
 import soundfile as sf
 
 
-def scaled_mel_filterbank(sample_rate, n_fft, n_mels, fmin, fmax):
+def scaled_mel_filters_filterbank(sample_rate, n_fft, n_mel_filters, fmin, fmax):
     """
-        Determines the matrix for a Mel-filterbank and scales it for each
+        Determines the matrix for a mel_filters-filterbank and scales it for each
         filter values to sum up to 1.
         
         :param sample_rate: sample rate of the signal
@@ -18,8 +18,8 @@ def scaled_mel_filterbank(sample_rate, n_fft, n_mels, fmin, fmax):
         :param n_fft: number of FFT components
         :type n_fft: int
 
-        :param n_mels: number of Mel bands to generate
-        :type n_mels: int
+        :param n_mel_filterss: number of mel_filters bands to generate
+        :type n_mel_filterss: int
 
         :param fmin: lowest frequency as a fraction of sample rate
         :type fmin: float
@@ -27,29 +27,29 @@ def scaled_mel_filterbank(sample_rate, n_fft, n_mels, fmin, fmax):
         :param fmax: highest frequency as a fraction of sample rate
         :type fmax: The second number to add
        
-        :return: Mel transform matrix
+        :return: mel_filters transform matrix
         :rtype: list of list of float64
     """
 
-    mel = filters.mel(sample_rate, n_fft, n_mels, fmin, fmax, 1)
+    mel_filters = filters.mel(sample_rate, n_fft, n_mel_filters, fmin, fmax, 1)
     
     M = []
-    for row in mel:
+    for row in mel_filters:
         row = list(row)
         m = min(x for x in row if x > 0)
         row_normalized = row / m
         row_scaled = row_normalized / row_normalized.sum()
         M.append(row_scaled)
-    mel_scaled = np.array(M)
+    mel_filters_scaled = np.array(M)
     
     # filterbank does not have first and last column in matlab code
-    #mel_scaled = mel_scaled[:,1:-1]
+    #mel_filters_scaled = mel_filters_scaled[:,1:-1]
     
-    return mel_scaled
+    return mel_filters_scaled
 
 
 def generate_features(audio_files, window_length=0.025, window_step=0.0125,
-                sample_rate=16000, use_spec_sub=False):
+                      sample_rate=16000, use_spec_sub=False):
     """
         Determines the energy and MFCCs of each signal.
         
@@ -83,7 +83,7 @@ def generate_features(audio_files, window_length=0.025, window_step=0.0125,
     F = []
     E = []
 
-    mel = scaled_mel_filterbank(16000, 400, 24, 0, sample_rate/2)
+    mel_filters = scaled_mel_filters_filterbank(16000, 400, 24, 0, sample_rate/2)
     
     i = 0
     while i < n_files:
@@ -104,14 +104,14 @@ def generate_features(audio_files, window_length=0.025, window_step=0.0125,
         
         # slide window over signal and compute energy and MFFCs at each step
         n_windows = s_length // window_step
-        signal_mfcc = np.zeros((n_windows, mel.shape[0]))
+        signal_mfcc = np.zeros((n_windows, mel_filters.shape[0]))
         signal_energy = np.zeros(n_windows)
         
         j = 0
         for i_start in range(0, s_length, window_step):
             frames = signal[i_start:i_start + window_length] * window_hamming
             fft_magnitude = np.abs(np.fft.rfft(frames))
-            mfcc = 20 * np.log10(np.matmul(mel, fft_magnitude))
+            mfcc = 20 * np.log10(np.matmul(mel_filters, fft_magnitude))
             
             signal_mfcc[j, :] = mfcc
             signal_energy[j] = sum(fft_magnitude)
